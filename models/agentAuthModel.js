255853d81fd557ca4f787f2a29b61ce5c64c72a9
ywonchae1 <ywonchae62@gmail.com>
2024-04-25 10:41:01 +0900

    feat: 공인중개사 계정 생성, 수정 구현

--- models/agentAuthModel.js
+++ models/agentAuthModel.js
@@ -1,13 +1,38 @@
 const Agent = require('../database/models/tables/agent');
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 module.exports = {
+  create: async (req, res) => {
+    const body = req.body;
+    try {
+      // 비밀번호 암호화해서 db에 저장하기
+      const passwordHash = await bcrypt.hash(body.password, saltRounds);
+
+      // 새로운 사용자 생성하기
+      const user = await Agent.create({
+        a_username: body.username,
+        a_password: passwordHash,
+        a_realname: body.realname,
+        a_email: body.email,
+        a_phone: body.phone,
+        agent_list_ra_regno: body.agentList_ra_regno,
+      });
+
+      // 새로 생성된 사용자 id 가져오기
+      // return result(user.r_username);
+    } catch (err) {
+      console.error("🚀 ~ err:", err);
+      // return result(null);
+    }
+  },
   findById: async (req, res) => {
     const username = req.body.username; // id는 로그인 시 사용하는 username
 
     try {
       const agent = await Agent.findAll({ where: {a_username: username}});
       if(agent) {
-        return res.json(agent.dataValues);
+        return res.json(agent);
       }
       return res.json({});
     } catch(err) {
@@ -15,11 +40,58 @@ module.exports = {
       return res.redirect('/');
     }
   },
+  update: async (req, res) => {
+    const body = req.body;
+    
+    try {
+      const agent = await Agent.update(
+        {
+          r_email: body.email,
+          r_phone: body.phone
+        },
+        { where: {
+          r_username: body.userId}
+        }
+      );
+      
+      return res.json(agent);
+    } catch(err) {
+      console.log('[error] auth DB : ', err);
+      return res.redirect('/');
+    }
+  },
+  updatepw: async (req, res) => {
+    const body = req.body;
+
+    try {
+      const passwordHash = await bcrypt.hash(body.password, saltRounds);
+
+      const agent = await Agent.findOne({
+        where: {a_username: body.userId}
+      });
+      
+      if(!agent) return res.redirect('');
+      const password = agent.dataValues.a_password;
+      const test = await bcrypt.compare(body.oldpassword, password);
+      if (!test) {
+        return res.json({});
+      } else {
+        await agent.update({
+          a_password: passwordHash
+        });
+        await agent.save();
+        return res.json(agent);
+      }
+    } catch (error) {
+      console.error(error);
+      return res.json({});
+    }
+  },
   delete: async (req, res) => {
     const a_username = req.body.username;
     try {
       await Agent.destroy({ where: {a_username: a_username} });
-      return 1;
+      return res.json({});
     } catch (error) {
       return error;
     }
--- routers/agentAuthRouter.js
+++ routers/agentAuthRouter.js
@@ -2,7 +2,10 @@ const express = require('express')
 const router = express.Router();
 const agentAuthModel = require('../models/agentAuthModel.js');
 
+router.post('/create', agentAuthModel.create);
 router.post('/findById', agentAuthModel.findById);
-router.delete('/delete', agentAuthModel.delete);
+router.put('/update', agentAuthModel.update);
+router.put('/updatepw', agentAuthModel.updatepw);
+router.post('/delete', agentAuthModel.delete);
 
 module.exports = router
